ab8aa0138aec5462f52307fb7a50ee9b603a419c
Mangata-Dev <mangata.tony@gmail.com>
2019-01-10 09:09:15 +0400

    ajout de constructeur et fonction basique de Personnage

--- Personnage.php
+++ Personnage.php
@@ -1,5 +1,21 @@
 <?php
+
 class Personnage{
   
+  protected $vie = 100 ;
+  protected $atk = 20;
+  protected $nom;
+	
+	
+	public function __construct($nom){
+		
+		$this->nom = $nom ;
+	}
+	public function attaque($cible){
+		
+		$cible->vie -= $this->atk ;
+	
+	}
+	
 }
 ?>
